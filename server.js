2b485a9eb3835aa4d32cedd02c80ecac0d276e2a
MeganKR <megankaye2018@gmail.com>
2024-01-26 20:58:14 -0600

    added basic routes in server.js

--- server.js
+++ server.js
@@ -8,3 +8,17 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
+
+// "GET /notes should return the notes.html file"
+app.get('/notes', (req,res) => {
+    res.sendFile(path.join(__dirname, 'public/notes.html'));
+});
+
+// "GET * should return the index.html file"
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log('Listening on PORT 3001');
+});
