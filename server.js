d7dc069e316bff3549a0404bd7162b50804c4ae9
MeganKR <megankaye2018@gmail.com>
2024-02-04 15:49:48 -0600

    added API routes:

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
+// package to generate unqiue IDs
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -11,27 +14,40 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 // "GET /notes should return the notes.html file"
-app.get('/notes', (req,res) => {
+app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/notes.html'));
 });
 
-// NOT DONE *****
 // "GET /api/notes should read the db.json file and return all saved notes as JSON"
-app.get('api/notes', (req, res) => {
-    res.json(notesData);
+app.get('/api/notes', (req, res) => {
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) throw err
+        let notesData = JSON.parse(data)
+        console.log(notesData);
+        res.json(notesData);
+    })
 });
 
-// NOT DONE *****
 // "POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client."
-app.post('api/notes', (req, res) => {
-
+app.post('/api/notes', (req, res) => {
+    const note = req.body;
+    note.id = uuidv4();
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) throw err
+        let notesData = JSON.parse(data)
+        notesData.push(note)
+        fs.writeFile('./db/db.json', JSON.stringify(notesData), (err) => {
+            if (err) throw err
+            res.status(200).json(notesData)
+        })
+    })
 });
 
-// NOT DONE *****
+// will try to finish if I have time
 // "DELETE /api/notes/:id should receive a query parameter containing the id of a note to delete."
-app.delete('api/notes/:id', (req, res) => {
-    notesData 
-});
+// app.delete('api/notes/:id', (req, res) => {
+//     notesData
+// });
 
 // "GET * should return the index.html file"
 app.get('*', (req, res) => {
